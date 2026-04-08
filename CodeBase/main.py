3203760dade549f698dacdf50840638adcb2318a
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2026-04-08 19:44:53 +0530

    Variables and Data Types

--- CodeBase/main.py
+++ CodeBase/main.py
@@ -1,5 +1,10 @@
+import variable_data_types
+
 def main():
     print("Hello Python, Here I Come")
+
+    #### 1.2 Variables and Data Types
+    variable_data_types.variables_data_types()
 
 if __name__ == "__main__":
     main()
